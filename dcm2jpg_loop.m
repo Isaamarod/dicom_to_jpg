dae9b9571a2fed3981f5bc6e0f7ebb1f34373e20
Isaamarod <belamarod@gmail.com>
2019-02-11 13:45:02 +0100

    modify dcm2jpg_loop.m

--- dcm2jpg_loop.m
+++ dcm2jpg_loop.m
@@ -2,7 +2,6 @@ clear all
 clc
 close all
 
-addpath('C:\Users\cober\Documents\MATLAB_DICOM_TO_JPG\export_fig-master');
 %based on https://es.mathworks.com/matlabcentral/fileexchange/49882-dicom-to-any-image-format-converter
 pathname_input= 'C:\Users\cober\Desktop\DICOM IMAGENES EN BRUTO\DCM_SIN';%DICOM path
 pathname_output= 'C:\Users\cober\Desktop\JPG IMAGENES EN BRUTO\JPG_SIN';%JPG outpath
@@ -30,15 +29,15 @@ jpgFiles = dir(filePattern_o); %todas las imagenes jpg.
 
 %for check loop
 
-for k = 1:length(filePattern_i) %recorre todas las imagenes
+for k = 1:length(dcmFiles) %recorre todas las imagenes
   baseFileName = dcmFiles(k).name;%nombre de la imagen
   fullFileName = fullfile(pathname_input, baseFileName); %coger la ruta de la imagen completa 'C:\Users\cober\Desktop\prueba_matlab\blur_20_RBG_-100_test_con'+nombre
   fprintf(1, 'Now reading %s\n', fullFileName); 
   %reading dicom image
   [X,MAP]=dicomread(fullFileName);
   image8 = uint8(255 * mat2gray(X));
-  %save output image jpg format 
-  %deleting .dcm in basefilename and change to .jpg
+  %save output image jpg format deleting .dcm in basefilename and change to
+  %.jpg
   baseFileName=strrep(baseFileName,'.dcm','.jpg');
   fullFileName_o = fullfile(pathname_output, baseFileName);
   imwrite(image8,fullFileName_o,'jpg');% Save Image as Jpeg format
